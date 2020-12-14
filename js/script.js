a6a53044b13128ba219194bc5ab3f50530065242
Lauren Crow <hi@lcrow.studio>
2020-12-14 10:37:58 +0100

    fix / operator

--- js/script.js
+++ js/script.js
@@ -107,7 +107,7 @@ class Calculator {
         case '*':
           computation = prev * current
           break
-        case '÷':
+        case '/':
           computation = prev / current
           break
         default:
